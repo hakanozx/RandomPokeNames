059d28ce5790c4edeed15b37683c7d411d77600d
Hakan Özcan <hakanozcan2534@gmail.com>
2023-09-16 18:27:16 +0300

    DEFINITELY FINISHED

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import pprint as pp
 import tkinter
+from tkinter import END, Text
 import random
 font = ('Arial', 30, "normal")
 font2 = ('Arial', 25, "bold")
@@ -33,8 +34,22 @@ for offset in range(0, 1000, 100):
 
 itemappended = random.choice(itemlist)
 
+def button_task():
+    text_box.delete('1.0', END)
+    itemappended = random.choice(itemlist)
+    text_box.insert(END, itemappended)
+
+
+
 my_label = tkinter.Label(text="Poke Names:" ,font=font)
 my_label.pack()
-my_label2 = tkinter.Label(text=itemappended, font=font2)
-my_label2.pack()
+text_box = tkinter.Text(window, height=10, width=50)
+text_box.pack()
+get_button = tkinter.Button(window, text="Get Poke", command=button_task)
+get_button.pack()
+#my_label2 = tkinter.Label(text=itemappended, font=font2)
+#my_label2.pack()
+
+
+
 window.mainloop()
