bc45e8ac7662842996fd8891f83b37090c2e0756
Hakan Özcan <hakanozcan2534@gmail.com>
2023-09-16 18:29:42 +0300

    DEFINITELY FINISHED 2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
-import pprint as pp
 import tkinter
-from tkinter import END, Text
+from tkinter import END
 import random
 font = ('Arial', 30, "normal")
 font2 = ('Arial', 25, "bold")
@@ -19,14 +18,13 @@ params = {'limit': 100}
 
 
 for offset in range(0, 1000, 100):
-    params['offset'] = offset  # add new value to dict with `limit`
+    params['offset'] = offset
     response = requests.get(url, params=params)
 
     if response.status_code != 200:
             print(response.text)
     else:
         data = response.json()
-        # pp.pprint(data)
         for item in data['results']:
             itemlist.append(item['name'])
 
@@ -47,8 +45,7 @@ text_box = tkinter.Text(window, height=10, width=50)
 text_box.pack()
 get_button = tkinter.Button(window, text="Get Poke", command=button_task)
 get_button.pack()
-#my_label2 = tkinter.Label(text=itemappended, font=font2)
-#my_label2.pack()
+
 
 
 
